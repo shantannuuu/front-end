607915588c539b984d4aafcaa595fd7ed0a3c6ec
shantannuuu <shantanu.p@elementree.co.in>
2020-11-19 16:52:57 +0530

    commit

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { MessageComponent } from './message/message.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { from } from 'rxjs';
+import { MessageService } from './services/message.service';
+import { HttpErrorHandler } from './services/http-error-handler.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    MessageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    HttpErrorHandler
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
